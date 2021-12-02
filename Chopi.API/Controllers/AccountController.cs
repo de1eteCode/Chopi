c0278f8a62b86cec98532a18f5ec798e238c444e
de1eteCode <skozlov2015@yandex.ru>
2021-12-02 10:29:36 +0500

    Вынос контроллер авторизации

--- Chopi.API/Controllers/AccountController.cs
+++ Chopi.API/Controllers/AccountController.cs
@@ -13,102 +13,83 @@ namespace Chopi.API.Controllers
     /// Функции для работы пользователя со своим аккаунтом
     /// </summary>
     
-    [ApiController]
-    [Route("[controller]")]
-    [Authorize]
-    public class AccountController : Controller
-    {
-        private readonly UserManager<User> _userManager;
-        private readonly SignInManager<User> _signInManager;
-        private readonly RoleManager<Role> _roleManager;
-        private readonly IUnitOfAccounts _unit;
+    //[ApiController]
+    //[Route("[controller]")]
+    //[Authorize]
+    //public class AccountController : Controller
+    //{
+    //    private readonly UserManager<User> _userManager;
+    //    private readonly SignInManager<User> _signInManager;
+    //    private readonly RoleManager<Role> _roleManager;
+    //    private readonly IUnitOfAccounts _unit;
 
-        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, RoleManager<Role> roleManager, IUnitOfAccounts unit)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _roleManager = roleManager;
-            _unit = unit;
-        }
+    //    public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, RoleManager<Role> roleManager, IUnitOfAccounts unit)
+    //    {
+    //        _userManager = userManager;
+    //        _signInManager = signInManager;
+    //        _roleManager = roleManager;
+    //        _unit = unit;
+    //    }
 
-        [HttpPost("login")]
-        [AllowAnonymous]
-        public async Task<IActionResult> Login([FromBody] LoginModel model)
-        {
-            var user = await _userManager.FindByNameAsync(model.Username);
+        
 
-            if (user is null || await _userManager.CheckPasswordAsync(user, model.Password) is false)
-            {
-                return Unauthorized();
-            }
+    //    [HttpPost("register")]
+    //    [AllowAnonymous]
+    //    public async Task<IActionResult> Register([FromBody] RegisterModel model)
+    //    {
+    //        throw new NotImplementedException("необходимо переделать метод, или контроллер по регистрации персонала/клиентов или" +
+    //            " сделать отдельный контроллер для админов/директоров чтобы они могли регистрировать как клиентов так и персонал, или только персонал и" +
+    //            " отдельный контроллер для самостоятельной регистрации клиентов");
 
-            await _signInManager.SignInAsync(user, false);
+    //        if (await _userManager.FindByNameAsync(model.Username) is not null
+    //            || await _userManager.FindByEmailAsync(model.Email) is not null)
+    //        {
+    //            return BadRequest();
+    //        }
 
-            return Ok();
-        }
+    //        var user = new User()
+    //        {
+    //            Email = model.Email,
+    //            UserName = model.Username,
+    //            NormalizedUserName = model.Username.Normalize(),
+    //            Passport = new Passport()
+    //            {
+    //                Citizenship = model.Citizenship,
+    //                SecondName = model.SecondName,
+    //                FirstName = model.FirstName,
+    //                MiddleName = model.MiddleName,
+    //                DateOfBirth = model.DateOfBirth,
+    //                Series = model.Series,
+    //                Number = model.Number,
+    //                ResidenceRegistration = model.ResidenceRegistration
+    //            }
+    //        };
 
-        [HttpPost("register")]
-        [AllowAnonymous]
-        public async Task<IActionResult> Register([FromBody] RegisterModel model)
-        {
-            throw new NotImplementedException("необходимо переделать метод, или контроллер по регистрации персонала/клиентов или" +
-                " сделать отдельный контроллер для админов/директоров чтобы они могли регистрировать как клиентов так и персонал, или только персонал и" +
-                " отдельный контроллер для самостоятельной регистрации клиентов");
-
-            if (await _userManager.FindByNameAsync(model.Username) is not null
-                || await _userManager.FindByEmailAsync(model.Email) is not null)
-            {
-                return BadRequest();
-            }
-
-            var user = new User()
-            {
-                Email = model.Email,
-                UserName = model.Username,
-                NormalizedUserName = model.Username.Normalize(),
-                Passport = new Passport()
-                {
-                    Citizenship = model.Citizenship,
-                    SecondName = model.SecondName,
-                    FirstName = model.FirstName,
-                    MiddleName = model.MiddleName,
-                    DateOfBirth = model.DateOfBirth,
-                    Series = model.Series,
-                    Number = model.Number,
-                    ResidenceRegistration = model.ResidenceRegistration
-                }
-            };
-
-            var result = await _userManager.CreateAsync(user, model.Password);
+    //        var result = await _userManager.CreateAsync(user, model.Password);
 
 
-            if (result.Succeeded)
-            {
-                var loging = new LoginModel()
-                {
-                    Username = model.Username,
-                    Password = model.Password
-                };
-                return await Login(loging);
-            }
-            else
-            {
-                return BadRequest();
-            }
-        }
+    //        if (result.Succeeded)
+    //        {
+    //            var loging = new LoginModel()
+    //            {
+    //                Username = model.Username,
+    //                Password = model.Password
+    //            };
+    //            return RedirectToAction("login", "auth", loging);
+    //            //return await Login(loging);
+    //        }
+    //        else
+    //        {
+    //            return BadRequest();
+    //        }
+    //    }
 
-        [HttpPost("logout")]
-        public async Task<IActionResult> Logout()
-        {
-            await _signInManager.SignOutAsync();
-            return Ok();
-        }
 
-        [HttpGet("getinfo")]
-        public async Task<IActionResult> GetPassport()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            return Ok(user.Passport);
-        }
-    }
+    //    [HttpGet("getinfo")]
+    //    public async Task<IActionResult> GetPassport()
+    //    {
+    //        var user = await _userManager.GetUserAsync(User);
+    //        return Ok(user.Passport);
+    //    }
+    //}
 }
---
+++ Chopi.API/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using Chopi.Modules.EFCore.Entities.Identity;
+using Chopi.Modules.Share;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Chopi.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [AllowAnonymous]
+    public class AuthController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AuthController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Username);
+
+            if (user is null || await _userManager.CheckPasswordAsync(user, model.Password) is false)
+            {
+                return Unauthorized();
+            }
+
+            await _signInManager.SignInAsync(user, false);
+
+            return Ok();
+        }
+
+
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Ok();
+        }
+    }
+}
