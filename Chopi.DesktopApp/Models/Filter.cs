5e657bac591ec943e0a2a04df2a245be7cc3b8b7
de1eteCode <skozlov2015@yandex.ru>
2021-12-03 13:39:05 +0500

    Добавление на будущее: Фильтр

---
+++ Chopi.DesktopApp/Models/Filter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chopi.DesktopApp.Models
+{
+    internal class Filter
+    {
+        public Filter(string title, string property)
+        {
+            Title = title;
+            Property = property;
+        }
+
+        public string Title { get; set; }
+        public string Property { get; set; }
+
+        public bool IsEqual<T>(T obj, string serach)
+            where T : class
+        {
+            if (obj is null)
+                return false;
+
+            if (string.IsNullOrEmpty(serach))
+                return true;
+
+            // todo: Реализовать фильтрацию
+
+            return true;
+        }
+    }
+}
