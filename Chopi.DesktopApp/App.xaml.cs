1a224d95c374e5386bae0639767c429895bc8274
de1eteCode <skozlov2015@yandex.ru>
2021-12-06 20:37:05 +0500

    Регистрация окон

--- Chopi.DesktopApp/App.xaml.cs
+++ Chopi.DesktopApp/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Chopi.DesktopApp.Core;
 using Chopi.DesktopApp.ViewModels;
 using Chopi.DesktopApp.Views;
+using Chopi.DesktopApp.Views.PermissionWindows;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -20,6 +21,12 @@ namespace Chopi.DesktopApp
         {
             Controller = new WindowController();
             Controller.RegisterVMToWindow<AuthVM, AuthWindow>();
+
+            Controller.RegisterVMToWindow<AccountantVM, AccountantWindow>();
+            Controller.RegisterVMToWindow<AdministratorVM, AdministratorWindow>();
+            Controller.RegisterVMToWindow<DirectorVM, DirectorWindow>();
+            Controller.RegisterVMToWindow<ManagerVM, ManagerWindow>();
+            Controller.RegisterVMToWindow<SystemAdministratorVM, SystemAdministratorWindow>();
         }
 
         public WindowController Controller { get; }
--- Chopi.DesktopApp/ViewModels/SystemAdministratorVM.cs
+++ Chopi.DesktopApp/ViewModels/SystemAdministratorVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Chopi.DesktopApp.ViewModels.Abstracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Chopi.DesktopApp.ViewModels
 {
-    internal class SystemAdministratorVM
+    internal class SystemAdministratorVM : BaseVM
     {
     }
 }
