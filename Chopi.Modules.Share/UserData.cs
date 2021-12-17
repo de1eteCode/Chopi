cd12b0248f8af6630ed58b31674059e6b020a6d7
de1eteCode <skozlov2015@yandex.ru>
2021-12-17 07:40:33 +0500

    Новая модель для передачи данных

---
+++ Chopi.Modules.Share/UserData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Chopi.Modules.Share
+{
+    public class UserData
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("username")]
+        public string UserName { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("phonenumber")]
+        public string PhoneNumber { get; set; }
+
+        [JsonPropertyName("firstname")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("secondname")]
+        public string SecondName { get; set; }
+
+        [JsonPropertyName("middlename")]
+        public string MiddleName { get; set; }
+
+        [JsonPropertyName("passportseria")]
+        public string Series { get; set; }
+
+        [JsonPropertyName("passportnumber")]
+        public string Number { get; set; }
+
+        [JsonPropertyName("passportregistration")]
+        public string ResidenceRegistration { get; set; }
+
+        [JsonPropertyName("citizenship")]
+        public string Citizenship { get; set; }
+
+        [JsonPropertyName("dateofbirth")]
+        public DateTime DateOfBirth { get; set; }
+
+        [JsonPropertyName("roles")]
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
